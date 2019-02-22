25044194e68ad6cfe6fbf4a42698703902b4cbd9
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-22 14:00:18 +0000

    Adds test ip address arr

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -11,4 +11,8 @@ describe Parser do
   it 'can return an array of urls' do
     expect(@parser.urls[0]).to eq('/help_page/1')
   end
+
+  it 'can return an array of ip addresses' do
+    expect(@parser.ips[0]).to eq('126.318.035.038')
+  end
 end
