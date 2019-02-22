3fd360302f6ddb6f98ff42dbc2a5c8fc174a74ec
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-22 14:02:26 +0000

    Move array construct to private method

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,6 +8,12 @@ class Parser
   end
 
   def parse
+    create_arrays
+  end
+
+  private
+
+  def create_arrays
     @file.readlines.each do |line|
       @urls << line.split(' ').first
       @ips << line.split(' ').last
