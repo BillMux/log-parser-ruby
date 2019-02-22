27077c179d3d5c015e9a9362270d2a70fc6d2de4
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-22 13:58:01 +0000

    Pushes urls and ips in seperate arrs

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,6 +3,13 @@ class Parser
   def initialize(filename)
     @file = File.open(filename)
     @urls  = []
+    @ips = []
   end
 
+  def parse
+    @file.readlines.each do |line|
+      @urls << line.split(' ').first
+      @ips << line.split(' ').last
+    end
+  end
 end
