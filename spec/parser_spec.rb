5065dbf98f2c05d984d467ef4a62242417dc2631
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-22 14:49:44 +0000

    Write unique visits test

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -30,8 +30,19 @@ describe Parser do
   it 'hashes all urls and their visits counts' do
     expect(@parser.show_visits).to eq(
       '/help_page/1' => 4,
-      '/contact' => 1,
       '/home' => 2,
+      '/contact' => 1,
+      '/about/2' => 1,
+      '/index' => 1,
+      '/about' => 1,
+    )
+  end
+
+  it 'can display results in order of unique visits' do
+    expect(@parser.show_unique_visits).to eq(
+      '/help_page/1' => 3,
+      '/contact' => 1,
+      '/home' => 1,
       '/about/2' => 1,
       '/index' => 1,
       '/about' => 1,
