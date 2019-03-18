57608ec8bc7da868de3603ecd3db0577e1255063
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-18 15:05:24 +0000

    Use subject instead of instance var

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -4,22 +4,23 @@ require_relative '../lib/parser'
 require 'rspec'
 
 describe Parser do
+  subject { Parser.new('src/testserver.log') }
+
   describe '#parse' do
     before(:each) do
-      @parser = Parser.new('src/testserver.log')
-      @parser.parse
+      subject.parse
     end
 
     it 'can return an array of urls' do
-      expect(@parser.urls[0]).to eq('/help_page/1')
+      expect(subject.urls[0]).to eq('/help_page/1')
     end
 
     it 'can return an array of ip addresses' do
-      expect(@parser.ips[0]).to eq('126.318.035.038')
+      expect(subject.ips[0]).to eq('126.318.035.038')
     end
 
     it 'puts urls and addresses into hash' do
-      expect(@parser.url_ip_pairs[0]).to eq(
+      expect(subject.url_ip_pairs[0]).to eq(
         '/help_page/1' => '126.318.035.038'
       )
     end
